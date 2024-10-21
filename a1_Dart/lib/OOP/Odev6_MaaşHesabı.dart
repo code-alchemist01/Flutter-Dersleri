1837746a37d12191bba9ab021c4c0936835a58cd
kutay <ibrahimkutaysahin765@gmail.com>
2024-10-22 00:04:22 +0300

    .

---
+++ a1_Dart/lib/OOP/Odev6_MaaşHesabı.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main() {
+  print("Kaç gün çalışıldı : ");
+  int mesaiG = int.parse(stdin.readLineSync()!);
+
+
+  print("$mesaiG gün çalıştığın için ${maasHesabi(mesaiG)} tl para kazandınız");
+}
+
+int maasHesabi(int gun) {
+
+  int toplamSaat = gun * 8;
+  if(toplamSaat > 160 ){
+    return (160 * 10) + ((toplamSaat - 160) * 20);
+
+  }
+  else if (toplamSaat <= 160) {
+    return toplamSaat * 10;
+  }
+  return 0;;
+}
