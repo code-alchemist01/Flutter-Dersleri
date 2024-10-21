847d235f9a16f3beb1e8ef81a6d59bd3372091dd
kutay <ibrahimkutaysahin765@gmail.com>
2024-10-22 02:36:05 +0300

    .

---
+++ a1_Dart/lib/OOP/a7_Constructor.dart
@@ -0,0 +1,14 @@
+/*
+
+  Bir sınıftan nesne oluşturmak için gerekli olan yapıdır
+  Varsayılan olarak biz oluşturmasakta oluşturur
+  Nesne oluşurkem istenilen kodlamalar bu metod içinde yapılabilir
+ */
+
+class Ogrenciler {
+  int no;
+  String ad;
+
+  Ogrenciler({required this.no,required this.ad}); // Dolu constructor
+
+}
---
+++ a1_Dart/lib/OOP/a7_ConstructorMain.dart
@@ -0,0 +1,11 @@
+import 'package:a1_dart_dili/OOP/a7_Constructor.dart';
+
+void main() {
+  var ogrenci = Ogrenciler(no: 100,ad: "Kutay");
+
+  /*ogrenci.no = 100;
+  ogrenci.ad = "Kutay";*/
+
+  print(ogrenci.ad);
+  print(ogrenci.no);
+}
