cb560a1fddb273440ec792dd5d63c60afe2e963e
kutay <ibrahimkutaysahin765@gmail.com>
2024-11-05 02:54:42 +0300

    Axis Alignment kullanımı Eklendi

---
+++ main_axis_alignment/android/app/src/main/kotlin/com/example/main_axis_alignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.main_axis_alignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ main_axis_alignment/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+
+      // mainAxisAlignment : Row veya Column içinde Row ise yatayda Column ise dikeyde hizalama yapar
+      // CrossAxisAlignment : MainAxis'in tersi yönde hizalama yapar. Row veya Column içinde rowsa dikeyde,column ise yatayda hizalama yapılır (ekstra stretch var)
+      // MainAxisSize : Row veya Column içinde bulunduğu alanın boyutunu belirler. MainAxisSize.max aslında match parent, MainAxisSize.min aslında wrap contenttir
+
+      // start(varsayılan), end(sona doğru), center(merkezde), spaceBetween,spaceEvenly şeklinde hizalar
+      body: Row(
+        //mainAxisAlignment: MainAxisAlignment.center, // merkeze hizalama yapar ( centern yerine kullanabildiğimiz her şeyi kullanabiliriz
+        //crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min, // örnek kullanımı
+        children: [
+          Container(width: 100,height: 100, color: Colors.lightBlue,),
+          Container(width: 50,height: 50, color: Colors.redAccent,),
+          Container(width: 100,height: 100, color: Colors.greenAccent,),
+        ],
+      ),
+
+    );
+  }
+}
