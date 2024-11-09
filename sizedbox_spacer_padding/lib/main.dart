daf50cae2f43d9e2afcb27ab723bdc55b26b1d2d
kutay <ibrahimkutaysahin765@gmail.com>
2024-11-09 16:20:01 +0300

    Sizedbox,spacer,padding ekledi

--- sizedbox_spacer_padding/lib/main.dart
+++ sizedbox_spacer_padding/lib/main.dart
@@ -42,8 +42,50 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body:
-a
+      body: Row( /* Padding*/
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(22.0), // her kenardan 22 lik boşluk oluşturmaya yarar
+            child: Container(width: 100,height: 100,color: Colors.red,),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20,right: 15), // belirtilen kenarlara boşluk uygular
+            child: Container(width: 100,height: 100,color: Colors.green,),
+          ),
+          Container(width: 100,height: 100,color: Colors.yellow,),
+        ],
+
+      ),
+      
+
+
+      /*Column( /* Spacer */
+        children: [
+          Container(width: 100,height: 100,color: Colors.red,),
+          Spacer(flex : 30), // var olan tüm boşluğu kullanır
+          Container(width: 80,height: 80,color: Colors.green,),
+          Spacer(flex : 70), // oranlayarak yüzden diliminde boşluk kullanabiliriz
+          Container(width: 80,height: 80,color: Colors.yellow,),
+        ],
+      ),*/
+
+
+      /* SizedBox()
+
+      Row(
+        children: [
+          Container(width: 100,height: 100,color: Colors.red,),
+          SizedBox( // sizedbox özelliği ile yeniden boyutlandırma yapabiliriz
+            width: 100,
+              height: 500,
+              child: Container(width: 50,height: 50,color: Colors.blue,)
+          ),
+          Container(width: 80,height: 80,color: Colors.green,),
+          SizedBox(width: 100,height: 100,), // boşluk yaratmamızı sağlar
+          Container(width: 80,height: 80,color: Colors.yellow,),
+        ],
+      ),*/
+
     );
   }
 }
